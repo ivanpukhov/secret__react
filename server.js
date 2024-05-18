0afea5056b63716d4353a644887cea10f3268fe8
ivaninbox <ix.pukhov@gmail.com>
2024-05-18 12:37:49 +0500

    f

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+const compression = require('compression');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+const BUILD_PATH = path.join(__dirname, 'build');
+
+app.use(compression()); // Сжатие ответов для уменьшения времени загрузки
+app.use(express.static(BUILD_PATH)); // Сервирование статических файлов из папки build
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(BUILD_PATH, 'index.html')); // Перенаправление всех запросов на index.html
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
